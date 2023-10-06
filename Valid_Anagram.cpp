f2b20c04c1776c2678cef3cc19c5d5d5efce0320
Raghunandan Bansal <raghubansal1102@gmail.com>
2023-10-07 02:41:02 +0530

    some dsa algorithms added

--- Valid_Anagram.cpp
+++ Valid_Anagram.cpp
@@ -1,19 +1,39 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        int freq[256]={0};
-        for(int i=0;i<s.size();i++){
-            freq[s[i]]++;
-        }
-        for(int i=0;i<t.size();i++){
-            freq[t[i]]--;
-        }
+#include<bits/stdc++.h>
+using namespace std;
 
-        for(int i=0;i<256;i++){
-            if(freq[i]!=0){
+// It has the boolean func. which first check the number of characters (length) of string, and if they are equal then it check each character of the strings.
+// we will sort the strings and they will be sorted according to their ascai value.
+bool anagram(string s1, string s2){
+    int n1= s1.length();
+    int n2= s2.length();
+
+    if(n1 != n2){
+        return false;
+    }
+    else{
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+    
+        for(int i=0; i<n1; i++){
+            if(s1[i] != s2[i]){
                 return false;
             }
         }
-        return true;
     }
-};
+    return true;
+}
+
+int main(){
+    string s1, s2;
+    cout<<"enter the first string : ";
+    cin>>s1;
+    cout<<"enter the second string : ";
+    cin>>s2;
+
+    if(anagram( s1, s2)){
+        cout<<"both the entered strings are anagram"<<endl;;
+    }
+    else{
+        cout<<"entered strings are not anagram"<<endl;
+    }
+}
