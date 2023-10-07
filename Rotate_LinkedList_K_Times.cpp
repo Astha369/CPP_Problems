d6ee262c5a4617db4b3d8cd764581becf6e72ba0
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2023-10-07 16:41:34 +0530

    Update Rotate_LinkedList_K_Times.cpp

--- Rotate_LinkedList_K_Times.cpp
+++ Rotate_LinkedList_K_Times.cpp
@@ -1,107 +1,60 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-};
-//left rotation by k times
-/*
-class Solution
-{
+
+class ListNode {
     public:
-    //Function to rotate a linked list.
-    Node* rotate(Node* head, int k)
-    {
-        Node *ptr=head,*head1;
-        int count=0;
-        while(ptr!=NULL){
-            count++;
-            ptr=ptr->next;
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+  ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL) return head;
+        int len = 0;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            len++;
+            temp = temp->next;
         }
-        if(count==k || k%count==0)
-        return head;
-        count=k%count;
-        ptr=head;
-        while(ptr->next){
-            if(count==1){
-                head1=ptr->next;
-                ptr->next=NULL;
-                ptr=head1;
-                count--;
-            }
-            else{
-                count--;
-                ptr=ptr->next;
-            }
+        ListNode* tail = head;
+        for(int i=1;i<len;i++){
+            tail = tail->next;
         }
-        ptr->next=head;
-        head=head1;
+        if(k == len) return head;
+        if(k>len) k%=len;
+        
+        int kk = len-k;
+        temp = head;
+        for(int i=1;i<kk;i++){
+            temp = temp->next;
+        }
+        tail->next = head;
+        head = temp->next;
+        temp->next = NULL;
         return head;
     }
-};*/
-  //right_rotation by k times
-class Solution {
-public:
-    Node* rotateRight(Node* head, int k) {
-        if(head==NULL || head->next==NULL)
-        return head;
-        Node *head1,*ptr=head,*temp;
-        int count=0;
-        while(ptr){
-            count++;
-            ptr=ptr->next;
-        }
-        if(k%count==0)
-        return head;
-        ptr=head;
-        count=count-(k%count);
-        // if(head->next->next==NULL)
-        // count-=1;
-        while(ptr->next!=NULL){
-            if(count==1){
-                head1=ptr->next;
-                ptr->next=NULL;
-                ptr=head1;
-                count--;
-                }
-                else{
-            count--;
-            ptr=ptr->next;
-                }
-        }
-        ptr->next=head;
-        head=head1;
-        return head;
+int main(){
+    ListNode* head = new ListNode(10);
+    int n;
+    cout<<"Enter value of n: ";
+    cin>>n;
+     cout<<"Enter value of ListNodes: \n";
+    ListNode* temp = head;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        ListNode* t = new ListNode(x);
+        temp->next = t;
+        temp = temp->next;
     }
-};
-void print(Node *head){
-      Node *ptr=head;
-      while(ptr!=NULL){
-            cout<<ptr->data<<" ";
-            ptr=ptr->next;
-      }
-      cout<<endl;
-      return;
-}
-int main( ){
-int n,num,k;
-cin>>n;
-Node *head,*tail,*temp;
-cin>>num;
-head=new Node(num);
-tail=head;
-for(int i=0;i<n-1;i++){
-      cin>>num;
-      tail->next=new Node(num);
-      tail=tail->next;
-}
-cin>>k;
-Solution ob;
-ob. rotateRight(head,k);
-print(head);
-return 0;
+    head = head->next;
+    int k;
+    cout<<"Enter value of k: ";
+    cin>>k;
+    head = rotateRight(head,k);
+    while(head!=NULL){
+        cout<<head->val<<" ";
+        head = head->next;
+    }
 }
