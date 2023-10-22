af358596f1f87bb3837e99dfe3efdba61abed643
mtgbischinu <113281125+mtgbischinu@users.noreply.github.com>
2023-10-22 21:53:55 +0530

    Create reverseinteger.cpp

---
+++ reverseinteger.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int y=0;
+        while(x){
+            if(y>INT_MAX/10 || y<INT_MIN/10){
+                return 0;
+            }else{
+                y=y*10 +x%10;
+                x=x/10;
+            }
+        }
+        return y;
+    }
+};
